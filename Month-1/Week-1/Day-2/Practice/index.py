78bd7e646e6518dce5b4951dbf69f1d142c29741
Prakash Kumar Jena <youremail@example.com>
2026-06-10 17:49:41 +0530

    assignment for Day 2

---
+++ Month-1/Week-1/Day-2/Practice/index.py
@@ -0,0 +1,42 @@
+# school="ABC School"
+# age=23
+# address="123 Main Street"
+# building_name="Sky Tower"
+
+
+# Data type 
+# Prmitive data type
+
+# name = "Suraj"
+# age = 23
+# cgpa = 9.5
+# is_valid = True
+
+# print(type(age))
+
+
+# name = input("Enter your full name : ")
+# age = input("Enter your age : ")
+# age_in_number = str(age)
+
+# print("Your age is : ", type(age_in_number))
+
+# Arithmetic operator
+
+# x = int(input("Enter the value of x : "))
+# y = int(input("Enter the value of y : "))
+
+
+# # addition
+# print("The sum of x and y is : ", x+y)
+
+
+# Logical operator
+
+# print(not True)
+
+# Comparison operator
+
+age = input("Enter your age : ")
+
+print( int(age) < 5)

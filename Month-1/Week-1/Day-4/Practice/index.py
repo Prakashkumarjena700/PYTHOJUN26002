d670f864b5985c9c0897f72eba492567495d06cb
Prakash Kumar Jena <youremail@example.com>
2026-06-13 17:41:06 +0530

    added day4, day5 chages

---
+++ Month-1/Week-1/Day-4/Practice/index.py
@@ -0,0 +1,68 @@
+# def aSimpleFunction():
+#     print("This is a simple function")
+    
+# aSimpleFunction()
+
+# Write a function for addition 2 numbers from input
+
+# def addTwoNumbers():
+#     a = input("Enter first number: ")
+#     b = input("Enter second number: ")
+    
+#     print("The sum of the two numbers is: ", int(a) + int(b))
+    
+# addTwoNumbers() 
+
+
+# def addTwoNumbers(a, b):
+#     print("The sum of the two numbers is: ", a + b)
+       
+# addTwoNumbers(5, 10)  
+
+# def runLoopFrom(x, y):
+#     for i in range(x, y):
+#         print(i)
+             
+# runLoopFrom(1,11)
+
+# def myReturnFunction():
+#     return "Hello world"
+    
+# op=myReturnFunction()
+
+# print(op)
+
+
+# def studentDetails():
+#     name = input("Enter your name : ")
+#     age = input("Enter your age : ")
+#     per = int(input("Enter your percentage : "))
+#     reg = input("Enter registration number : ")
+    
+#     print(name, age, per, reg)
+    
+    
+# studentDetails()    
+
+
+fruit  = "Apple"
+
+def printFruit():
+    
+    student_1 = "Wasim"
+    
+    print(fruit, student_1)
+
+
+print(fruit)
+
+# printFruit()
+
+
+# color option
+# red = "Stop"
+# orange = "Start"
+# green = "Go"
+
+# Make a function that takes color as parameter and return the result 
+
---
+++ Month-1/Week-1/Day-5/Practice/index.py
@@ -0,0 +1,57 @@
+# ice_cream=["vanella", "chocolate", "butterchoe","buleberry"]
+
+# print(ice_cream[1])
+# print(ice_cream[-1])
+
+# ice_cream.append("bulebeey")
+# ice_cream.insert(1, "buleberry")
+# ice_cream.remove("chocolate")
+# ice_cream.pop()
+# print(len(ice_cream))
+
+# for item in ice_cream:
+#     print(item)
+
+
+# building = ("building1", "building2", "building3","building3" )
+
+# print(building[0])
+# print(building)
+
+# num = {1,2,3,4,5}
+
+# num.add(6)
+# num.remove(4)
+
+# print(num)
+
+
+# school = {
+#     'name':'xyz School',
+#     'add': 'Bhubaneswar',
+#     'buildings':6,
+#     'city':'BBSR'
+# }
+
+# school_address = school['add']
+# school["students"] = 1000
+# # school.pop("city")
+
+# for key,val in school.items():
+#     print(key,"-",val)
+
+
+# String methods------------------
+
+name = "Sonali is a student"
+
+# print(name.upper())
+# print(name.lower())
+
+# for char in name:
+#     print(char)
+    
+name.replace("Sonali","Sarita")    
+
+print(name)
+
